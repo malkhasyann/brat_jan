7d9e52425bd68e0bc52aa100ad6fe84ed412f2a5
malkhasyan <ara.malkhasyan2019@gmail.com>
2022-02-19 21:59:15 +0400

    BUG FIXING
    Added value assignment without declaration

--- brat_jan.py
+++ brat_jan.py
@@ -1,6 +1,7 @@
 """ Brat jan! This is the main module! """
 
 import errors
+import states
 from validity import *
 import write
 import expressions
@@ -16,8 +17,11 @@ if __name__ == "__main__":
         source_code = file.readlines()
 
     for line in source_code:
-        if line.startswith('bratwrite'):
+        if line.startswith('bratwrite'):  # new variable is declared and assigned
             write.execute_bratwrite(line)
-        if line.startswith('bratjan'):
+        if line.startswith('bratjan'):  # printing
             expressions.execute_declaration_assignment(line)
-
+        if line.startswith('#'):  # comment line in brat jan language starts wit '#'
+            continue
+        if line.split()[0] in states.namespace and '=' in line:
+            expressions.execute_assignment(line)
